81cc9e8682a66296d5ef69a7f253d4a2ec5dfd36
aun1414 <abbasaun1414@gmail.com>
2022-09-18 21:05:05 +0500

    Initial commit

---
+++ app/src/main/java/com/syedaunabbas/i190435/MainActivity20.java
@@ -0,0 +1,14 @@
+package com.syedaunabbas.i190435;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity20 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main20);
+    }
+}
